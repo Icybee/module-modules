1eaa89d33d289415eb22fc8d9a2217de3ee6c832
Olivier Laviale <olivier.laviale@gmail.com>
2015-07-27 00:15:29 +0200

    Using new routing features

--- config/routes.php
+++ config/routes.php
@@ -2,18 +2,44 @@
 
 namespace Icybee\Modules\Modules;
 
+use ICanBoogie\HTTP\Request;
+use Icybee\Routing\RouteMaker as Make;
+
+return Make::admin('modules', ManageController::class, [
+
+	'only' => [ 'index', 'inactive', 'install' ],
+	'actions' => [
+
+		'inactive' => [ '/{name}/inactive', Request::METHOD_ANY ],
+		'install' => [ '/{name}/<[^/]+>/install', Request::METHOD_ANY ]
+
+	]
+
+]) + [
+
+	'redirect:admin/features' => [
+
+		'pattern' => '/admin/features',
+		'location' => '/admin/modules'
+
+	]
+
+];
+
+//var_dump($routes);
+
 return [
 
 	'!admin:manage' => [
 
-		'controller' => __NAMESPACE__ . '\ManageController'
+		'controller' => ManageController::class
 
 	],
 
 	'admin:modules/inactives' => [
 
 		'pattern' => '/admin/modules/inactives',
-		'controller' => __NAMESPACE__ . '\ManageController',
+		'controller' => ManageController::class,
 		'title' => 'Inactives',
 		'block' => 'inactives'
 
@@ -22,7 +48,7 @@ return [
 	'admin:modules/install' => [
 
 		'pattern' => '/admin/modules/<[^/]+>/install',
-		'controller' => __NAMESPACE__ . '\ManageController',
+		'controller' => ManageController::class,
 		'title' => 'Install',
 		'block' => 'install',
 		'visibility' => 'auto'
