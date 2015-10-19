5a4cd51d8d2b4ef8e16cbba8c7ba602410a60f0b
Olivier Laviale <olivier.laviale@gmail.com>
2015-10-19 23:49:19 +0200

    Tidying

--- lib/Routing/ModulesAdminController.php
+++ lib/Routing/ModulesAdminController.php
@@ -13,14 +13,14 @@ namespace Icybee\Modules\Modules\Routing;
 
 use ICanBoogie\Errors;
 use ICanBoogie\HTTP\Request;
+use ICanBoogie\Module;
 
 use Icybee\Binding\PrototypedBindings;
 use Icybee\Routing\AdminController;
 
 class ModulesAdminController extends AdminController
 {
-	use PrototypedBindings;
-	use \ICanBoogie\Module\CoreBindings;
+	use PrototypedBindings, Module\CoreBindings;
 
 	/**
 	 * Clears module cache before doing anything.
@@ -29,11 +29,15 @@ class ModulesAdminController extends AdminController
 	 */
 	protected function action(Request $request)
 	{
-		$app = $this->app;
-
-		if ($app->has_property('cache'))
+		try
 		{
-			$app->cache['core.modules']->clear();
+			$this->app->caches['core.modules']->clear();
+		}
+		catch (\Exception $e)
+		{
+			#
+			# Not important
+			#
 		}
 
 		return parent::action($request);
@@ -43,6 +47,7 @@ class ModulesAdminController extends AdminController
 	 * @param $module_id
 	 *
 	 * @return \ICanBoogie\HTTP\RedirectResponse
+	 *
 	 * @throws \Exception
 	 */
 	protected function action_install($module_id)
